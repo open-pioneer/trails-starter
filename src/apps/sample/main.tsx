3d8e50aaadf3669d4c2907ab9e6b151aca7c89a3
Michael Beckemeyer <m.beckemeyer@conterra.de>
2023-01-16 09:39:14 +0100

    Setup minimal workspaces

--- src/apps/sample/main.tsx
+++ src/apps/sample/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "../../index.css";
+
+import { hello } from "hello-world/hello";
+
+hello();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
