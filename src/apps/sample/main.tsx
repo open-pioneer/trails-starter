15e45f7a4cb208778eb359f602708890889df496
Michael Beckemeyer <m.beckemeyer@conterra.de>
2023-01-18 10:21:06 +0100

    Initialize custom element factory

--- src/apps/sample/main.tsx
+++ src/apps/sample/main.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+import { createCustomElement } from "@open-pioneer/runtime";
 
-import { hello } from "hello-world/hello";
+// import { hello } from "hello-world/hello";
 
-hello();
+// hello();
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// );
+
+const CustomElementClazz = createCustomElement({
+    component: <div>Hello World!</div>,
+    styles: "",
+    openShadowRoot: true
+});
+globalThis.customElements.define("sample-element", CustomElementClazz);
