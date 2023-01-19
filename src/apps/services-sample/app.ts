3b325fda21c6de72ce1d84e27114b195c9442170
Jan Schulte <j.schulte@52north.org>
2023-01-19 15:31:10 +0100

    support startup for service layer

---
+++ src/apps/services-sample/app.ts
@@ -0,0 +1,64 @@
+import { Service, createCustomElement, ServiceOptions } from "@open-pioneer/runtime";
+import { BundleMetadata } from "@open-pioneer/runtime/Metadata";
+import { ServiceLayer } from "@open-pioneer/runtime/services/ServiceLayer";
+
+interface UserReferences {
+    provider: ProviderInterface;
+}
+
+interface ProviderInterface {
+    sayHello(): void;
+}
+
+const bundles: Record<string, BundleMetadata> = {
+    "test-bundle1": {
+        name: "test-bundle1",
+        services: {
+            User: {
+                name: "User",
+                clazz: class User implements Service {
+                    #provider: ProviderInterface;
+                    constructor(options: ServiceOptions<UserReferences>) {
+                        this.#provider = options.references.provider;
+                        this.#provider.sayHello();
+                    }
+                    destroy(): void {
+                        console.log("User destroy");
+                    }
+                },
+                references: {
+                    provider: {
+                        interface: "test-bundle2.SomeService"
+                    }
+                }
+            }
+        }
+    },
+    "test-bundle2": {
+        name: "test-bundle2",
+        services: {
+            Provider: {
+                name: "Provider",
+                clazz: class Provider implements Service<ProviderInterface> {
+                    sayHello() {
+                        console.log("Hello from service of test-bundle2!");
+                    }
+                },
+                provides: [
+                    {
+                        interface: "test-bundle2.SomeService"
+                    }
+                ]
+            }
+        }
+    }
+};
+
+const Element = createCustomElement({
+    component: "test",
+    bundles
+});
+
+new ServiceLayer(bundles).start();
+
+customElements.define("services-sample", Element);
