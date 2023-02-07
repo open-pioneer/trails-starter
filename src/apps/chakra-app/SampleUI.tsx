c3a20ff8d094c1741f76cf9e3fa2e8d984f5c554
Michael Beckemeyer <m.beckemeyer@conterra.de>
2023-02-07 11:04:35 +0100

    Wrap portal-using chakra components

--- src/apps/chakra-app/SampleUI.tsx
+++ src/apps/chakra-app/SampleUI.tsx
@@ -1,9 +1,80 @@
-import { Button, Container } from "@open-pioneer/chakra-integration";
+import {
+    Button,
+    Container,
+    Drawer,
+    DrawerBody,
+    DrawerCloseButton,
+    DrawerContent,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    Input,
+    useDisclosure,
+    useToast
+} from "@open-pioneer/chakra-integration";
+import { useRef } from "react";
 
 export function SampleUI() {
     return (
         <Container>
-            <Button>Hello World!</Button>
+            <ToastExample />
+            <DrawerExample />
         </Container>
+    );
+}
+
+function ToastExample() {
+    const toast = useToast();
+    return (
+        <Button
+            onClick={() =>
+                toast({
+                    title: "Account created.",
+                    description: "We've created your account for you.",
+                    status: "success",
+                    duration: 9000,
+                    position: "bottom-left",
+                    isClosable: true
+                })
+            }
+        >
+            Show Toast
+        </Button>
+    );
+}
+
+function DrawerExample() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const btnRef = useRef<HTMLButtonElement>(null);
+    return (
+        <>
+            <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
+                Open
+            </Button>
+            <Drawer
+                isOpen={isOpen}
+                placement="right"
+                onClose={onClose}
+                finalFocusRef={btnRef}
+                isFullHeight={false}
+            >
+                <DrawerOverlay />
+                <DrawerContent>
+                    <DrawerCloseButton />
+                    <DrawerHeader>Create your account</DrawerHeader>
+
+                    <DrawerBody>
+                        <Input placeholder="Type here..." />
+                    </DrawerBody>
+
+                    <DrawerFooter>
+                        <Button variant="outline" mr={3} onClick={onClose}>
+                            Cancel
+                        </Button>
+                        <Button colorScheme="blue">Save</Button>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+        </>
     );
 }
