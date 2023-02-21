a96b68875eedc610b579a97d708297ce4c8c803d
Antonia van Eek <a.vaneek@conterra.de>
2023-02-21 16:46:31 +0100

    Logger for standardized application wide logging

--- src/apps/api-app/TextApiExtension.ts
+++ src/apps/api-app/TextApiExtension.ts
@@ -1,6 +1,7 @@
 import { ServiceOptions } from "@open-pioneer/runtime";
 import { ApiExtension } from "@open-pioneer/integration";
 import { TextService } from "./TextService";
+import { createLogger, Logger } from "@open-pioneer/core";
 
 interface References {
     textService: TextService;
@@ -8,14 +9,17 @@ interface References {
 
 export class TextApiExtension implements ApiExtension {
     private textService: TextService;
+    private logger: Logger;
     constructor(opts: ServiceOptions<References>) {
         this.textService = opts.references.textService;
+        this.logger = createLogger("test-prefix");
     }
 
     async getApiMethods() {
         return {
             changeText: (text: string) => {
                 this.textService.setText(text);
+                this.logger.warn("api-app:TextApiExtension", { testLog: 123, text: text });
             }
         };
     }
