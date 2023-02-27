299f7a0b9e67632f4e4241a6a309be321a4b35b8
Michael Beckemeyer <m.beckemeyer@conterra.de>
2023-02-27 13:31:28 +0100

    Use ./services to export service classes, and upgrade to latest build tools

--- src/samples/extension-sample/extension-app/index.ts
+++ src/samples/extension-sample/extension-app/index.ts
@@ -1,7 +1,3 @@
 // SPDX-FileCopyrightText: con terra GmbH and contributors
 // SPDX-License-Identifier: Apache-2.0
 export * from "./api";
-export { ActionServiceImpl } from "./ActionsServiceImpl";
-export { LoggingActionProvider } from "./providers/LoggingActionProvider";
-export { MultiActionProvider } from "./providers/MultiActionProvider";
-export { OpenWindowActionProvider } from "./providers/OpenWindowActionProvider";
---
+++ src/samples/extension-sample/extension-app/services.ts
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: con terra GmbH and contributors
+// SPDX-License-Identifier: Apache-2.0
+export { ActionServiceImpl } from "./ActionsServiceImpl";
+export { LoggingActionProvider } from "./providers/LoggingActionProvider";
+export { MultiActionProvider } from "./providers/MultiActionProvider";
+export { OpenWindowActionProvider } from "./providers/OpenWindowActionProvider";
