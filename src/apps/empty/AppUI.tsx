a45454e67d05fa7445483ad74e15cb3f9c65645b
t.stein <t.stein@conterra.de>
2023-03-07 15:29:48 +0100

    i18n added to empty app

--- src/apps/empty/AppUI.tsx
+++ src/apps/empty/AppUI.tsx
@@ -1,14 +1,16 @@
 // SPDX-FileCopyrightText: con terra GmbH and contributors
 // SPDX-License-Identifier: Apache-2.0
 import { Container, Heading, Text } from "@open-pioneer/chakra-integration";
+import { useIntl } from "open-pioneer:react-hooks";
 
 export function AppUI() {
+    const intl = useIntl();
     return (
         <Container>
             <Heading as="h1" size="lg">
-                Empty App
+                {intl.formatMessage({ id: "heading" })}
             </Heading>
-            <Text>This is an empty app.</Text>
+            <Text>{intl.formatMessage({ id: "text" })}</Text>
         </Container>
     );
 }
