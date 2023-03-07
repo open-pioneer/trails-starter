1e61bb18e69c9f177d92a14ef713b2f4d366339b
t.stein <t.stein@conterra.de>
2023-03-07 14:58:52 +0100

    extended i18n demo

--- src/samples/i18n-sample/i18n-app/I18nUI.tsx
+++ src/samples/i18n-sample/i18n-app/I18nUI.tsx
@@ -21,6 +21,8 @@ export function I18nUI() {
     const appCtx = useService("runtime.ApplicationContext");
     const locale = appCtx.getLocale();
     const supportedLocales = appCtx.getSupportedLocales();
+    const name = "Müller";
+    const list = ["Hans", "Peter", "Hape"];
 
     return (
         <Container>
@@ -33,8 +35,61 @@ export function I18nUI() {
             <UnorderedList mb={4}>
                 <ListItem>Current locale: {locale}</ListItem>
                 <ListItem>Supported locales: {supportedLocales.join(", ")}</ListItem>
-                <ListItem>Current date: {intl.formatDate(new Date())}</ListItem>
-                <ListItem>Large number: {intl.formatNumber(1234567.89)}</ListItem>
+                <ListItem>
+                    Current date and time:{" "}
+                    {intl.formatDate(new Date(), { dateStyle: "full", timeStyle: "short" })}
+                </ListItem>
+                <ListItem>
+                    Relative Time - 1:{" "}
+                    {intl.formatRelativeTime(1, "minute", { numeric: "auto", style: "long" })}
+                </ListItem>
+                <ListItem>
+                    Relative Time - 15:{" "}
+                    {intl.formatRelativeTime(15, "minute", { numeric: "auto", style: "long" })}
+                </ListItem>
+                <ListItem>
+                    Relative Time - 0:{" "}
+                    {intl.formatRelativeTime(0, "minute", { numeric: "auto", style: "long" })}
+                </ListItem>
+                <ListItem>
+                    Large number (Currency):{" "}
+                    {intl.formatNumber(1234567.89, { style: "currency", currency: "EUR" })}
+                </ListItem>
+                <ListItem>
+                    Large number (Unit):{" "}
+                    {intl.formatNumber(1234567.89, { style: "unit", unit: "kilogram-per-second" })}
+                </ListItem>
+                <ListItem>
+                    Plural - Count 0: {intl.formatMessage({ id: "content.testplural" }, { n: 0 })}
+                </ListItem>
+                <ListItem>
+                    Plural - Count 1: {intl.formatMessage({ id: "content.testplural" }, { n: 1 })}
+                </ListItem>
+                <ListItem>
+                    Plural - Count 2: {intl.formatMessage({ id: "content.testplural" }, { n: 2 })}
+                </ListItem>
+                <ListItem>List: {intl.formatList(list, { type: "conjunction" })}</ListItem>
+                <ListItem>
+                    Gender - male:{" "}
+                    {intl.formatMessage(
+                        { id: "content.testgender" },
+                        { gender: "female", name: name }
+                    )}
+                </ListItem>
+                <ListItem>
+                    Gender - female:{" "}
+                    {intl.formatMessage(
+                        { id: "content.testgender" },
+                        { gender: "male", name: name }
+                    )}
+                </ListItem>
+                <ListItem>
+                    Gender - other:{" "}
+                    {intl.formatMessage(
+                        { id: "content.testgender" },
+                        { gender: "other", name: name }
+                    )}
+                </ListItem>
             </UnorderedList>
 
             <Center mb={4}>
