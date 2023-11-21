45ab8643c1a624cafed56ce24384d2854bf04fcd
Bierett, Danilo <danilo.bierett@it.nrw.de>
2023-11-21 08:49:04 +0100

    Add further packages

--- src/samples/map-sample/ol-app/MapApp.tsx
+++ src/samples/map-sample/ol-app/MapApp.tsx
@@ -1,20 +1,44 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
-import { Box, Flex } from "@open-pioneer/chakra-integration";
+import { Box, Flex, FormControl, FormLabel, Text } from "@open-pioneer/chakra-integration";
 import { MapAnchor, MapContainer } from "@open-pioneer/map";
 import { ScaleBar } from "@open-pioneer/scale-bar";
 import { InitialExtent, ZoomIn, ZoomOut } from "@open-pioneer/map-navigation";
 import { useIntl } from "open-pioneer:react-hooks";
 import { CoordinateViewer } from "@open-pioneer/coordinate-viewer";
-import { SectionHeading, TitledSection } from "@open-pioneer/react-utils";
+import { SectionHeading, TitledSection, ToolButton } from "@open-pioneer/react-utils";
 import { ScaleViewer } from "@open-pioneer/scale-viewer";
+import { Geolocation } from "@open-pioneer/geolocation";
+import { Notifier } from "@open-pioneer/notifier";
+import { OverviewMap } from "@open-pioneer/overview-map";
 import { MAP_ID } from "./services";
+import { useId, useMemo, useState } from "react";
+import TileLayer from "ol/layer/Tile";
+import { Measurement } from "@open-pioneer/measurement";
+import OSM from "ol/source/OSM";
+import { PiRulerLight } from "react-icons/pi";
+import { BasemapSwitcher } from "@open-pioneer/basemap-switcher";
 
 export function MapApp() {
     const intl = useIntl();
+    const measurementTitleId = useId();
+
+    const [measurementIsActive, setMeasurementIsActive] = useState<boolean>(false);
+    function toggleMeasurement() {
+        setMeasurementIsActive(!measurementIsActive);
+    }
+
+    const overviewMapLayer = useMemo(
+        () =>
+            new TileLayer({
+                source: new OSM()
+            }),
+        []
+    );
 
     return (
         <Flex height="100%" direction="column" overflow="hidden">
+            <Notifier position="bottom" />
             <TitledSection
                 title={
                     <Box
@@ -33,14 +57,71 @@ export function MapApp() {
                         role="main"
                         aria-label={intl.formatMessage({ id: "ariaLabel.map" })}
                     >
+                        <MapAnchor position="top-left" horizontalGap={5} verticalGap={5}>
+                            <Box
+                                backgroundColor="white"
+                                borderWidth="1px"
+                                borderRadius="lg"
+                                padding={2}
+                                boxShadow="lg"
+                                role="top-left"
+                                aria-label={intl.formatMessage({ id: "ariaLabel.topLeft" })}
+                            >
+                                {measurementIsActive && (
+                                    <Box role="dialog" aria-labelledby={measurementTitleId} mt={5}>
+                                        <TitledSection
+                                            title={
+                                                <SectionHeading
+                                                    id={measurementTitleId}
+                                                    size="md"
+                                                    mb={2}
+                                                >
+                                                    {intl.formatMessage({ id: "measurementTitle" })}
+                                                </SectionHeading>
+                                            }
+                                        >
+                                            <Measurement mapId={MAP_ID} />
+                                        </TitledSection>
+                                    </Box>
+                                )}
+                            </Box>
+                        </MapAnchor>
+                        <MapAnchor position="top-right" horizontalGap={5} verticalGap={5}>
+                            <Box
+                                backgroundColor="white"
+                                borderWidth="1px"
+                                borderRadius="lg"
+                                padding={2}
+                                boxShadow="lg"
+                                role="top-right"
+                                aria-label={intl.formatMessage({ id: "ariaLabel.topRight" })}
+                            >
+                                <OverviewMap mapId={MAP_ID} olLayer={overviewMapLayer} />
+                                <FormControl>
+                                    <FormLabel ps={1} pt={2}>
+                                        <Text as="b">
+                                            {intl.formatMessage({ id: "basemapLabel" })}
+                                        </Text>
+                                    </FormLabel>
+                                    <BasemapSwitcher mapId={MAP_ID} allowSelectingEmptyBasemap />
+                                </FormControl>
+                            </Box>
+                        </MapAnchor>
                         <MapAnchor position="bottom-right" horizontalGap={10} verticalGap={30}>
                             <Flex
-                                role="toolbar"
-                                aria-label={intl.formatMessage({ id: "ariaLabel.toolbar" })}
+                                role="bottom-right"
+                                aria-label={intl.formatMessage({ id: "ariaLabel.bottomRight" })}
                                 direction="column"
                                 gap={1}
                                 padding={1}
                             >
+                                <ToolButton
+                                    label={intl.formatMessage({ id: "measurementTitle" })}
+                                    icon={<PiRulerLight />}
+                                    isActive={measurementIsActive}
+                                    onClick={toggleMeasurement}
+                                />
+                                <Geolocation mapId={MAP_ID} />
                                 <InitialExtent mapId={MAP_ID} />
                                 <ZoomIn mapId={MAP_ID} />
                                 <ZoomOut mapId={MAP_ID} />
--- src/samples/map-sample/ol-app/services.ts
+++ src/samples/map-sample/ol-app/services.ts
@@ -1,30 +1,12 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 import { MapConfig, MapConfigProvider, SimpleLayer } from "@open-pioneer/map";
-import { Attribution } from "ol/control";
 import TileLayer from "ol/layer/Tile";
-import { MapOptions } from "ol/Map";
 import OSM from "ol/source/OSM";
-import View from "ol/View";
 
 export const MAP_ID = "main";
 export class MainMapProvider implements MapConfigProvider {
     mapId = MAP_ID;
-
-    mapOptions: MapOptions = {
-        view: new View({
-            projection: "EPSG:3857",
-            center: [847541, 6793584],
-            zoom: 14
-        }),
-        layers: [
-            new TileLayer({
-                source: new OSM(),
-                properties: { title: "OSM" }
-            })
-        ],
-        controls: [new Attribution()]
-    };
 
     async getMapConfig(): Promise<MapConfig> {
         return {
@@ -36,11 +18,12 @@ export class MainMapProvider implements MapConfigProvider {
             projection: "EPSG:3857",
             layers: [
                 new SimpleLayer({
-                    title: "Foo",
+                    title: "OpenStreetMap",
                     olLayer: new TileLayer({
                         source: new OSM(),
                         properties: { title: "OSM" }
-                    })
+                    }),
+                    isBaseLayer: true
                 })
             ]
         };
