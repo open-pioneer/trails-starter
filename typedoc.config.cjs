9b2c198bae29f09f37a451e1d6ff4bbe44c8774a
Danilo Bierett <danilo.bierett@it.nrw.de>
2024-11-29 09:07:31 +0100

    Fix deprecated eslint rule (#112)
    See https://typescript-eslint.io/rules/no-var-requires/

--- typedoc.config.cjs
+++ typedoc.config.cjs
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
 const { sync: fastGlobSync } = require("fast-glob");
 const { dirname } = require("path");
 const DEFAULT_HIGHLIGHT_LANGS = require("typedoc").OptionDefaults.highlightLanguages;
