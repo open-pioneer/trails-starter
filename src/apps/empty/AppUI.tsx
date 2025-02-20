0223d4b2900ffb5eba8406162ded4c3933a86473
Michael Beckemeyer <m.beckemeyer@conterra.de>
2025-02-20 15:50:56 +0100

    Update license headers

--- src/apps/empty/AppUI.tsx
+++ src/apps/empty/AppUI.tsx
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
+// SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 import { Container, Heading, Text, chakra } from "@open-pioneer/chakra-integration";
 import { useIntl, useService } from "open-pioneer:react-hooks";
