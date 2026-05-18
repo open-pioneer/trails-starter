d725c68d0af4e9b5e421f90d426aca46104b81a6
artmarks <98356887+artmarks@users.noreply.github.com>
2026-05-18 10:50:55 +0200

    Feature/hmr i18n (#145)

--- src/samples/i18n-howto/app/GreetingService.ts
+++ src/samples/i18n-howto/app/GreetingService.ts
@@ -1,20 +1,21 @@
 // SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 import { type DECLARE_SERVICE_INTERFACE, ServiceOptions, PackageIntl } from "@open-pioneer/runtime";
+import { ReadonlyReactive } from "@conterra/reactivity-core";
 
 export class GreetingService {
     declare [DECLARE_SERVICE_INTERFACE]: "i18n-howto-app.GreetingService";
 
-    private _intl: PackageIntl;
+    private readonly _intl: ReadonlyReactive<PackageIntl>;
 
     constructor(serviceOptions: ServiceOptions) {
-        this._intl = serviceOptions.intl;
+        this._intl = serviceOptions.currentIntl;
     }
 
     /**
      * Greets the user in the current locale.
      */
     greet(name: string): string {
-        return this._intl.formatMessage({ id: "greetingService.greeting" }, { name });
+        return this._intl.value.formatMessage({ id: "greetingService.greeting" }, { name });
     }
 }
